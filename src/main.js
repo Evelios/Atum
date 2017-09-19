cc26d1d0ef31263ff92e8bc339bfd25fc1778e2e
Thomas Waters <twaters117@gmail.com>
2017-09-19 12:06:59 -0400

    ES6 Basic Build System

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import Vector from "./utilities/Vector";
+import * as Redist from "./utilities/Redist";
+import * as Rand from "./utilities/Rand";
+
+/**
+ * The Atum procedural graph based library
+ * 
+ * @export
+ * @module Atum
+ * @see {@link https://github.com/Evelios/Atum}
+ */
+const Atum = {
+    Vector,
+    Redist,
+    Rand
+};
+
+console.log(Atum);
+
+// export default Atum;
