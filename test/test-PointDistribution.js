bd926e925ed05af4bf79a892e53cea4e91044b4a
Thomas Waters <twaters117@gmail.com>
2017-10-02 14:51:51 -0400

    Basic PointDistribution Test

--- test/test-PointDistribution.js
+++ test/test-PointDistribution.js
@@ -1,7 +1,26 @@
+"use strict";
+
+const test = require("tape");
 const Atum = require('../build/Atum').default;
+
 const PointDistribution = Atum.Utility.PointDistribution;
 const Vector = Atum.Geometry.Vector;
 const Rectangle = Atum.Geometry.Rectangle;
 
-const bbox = new Rectangle(Vector.zero(), 800, 1000);
-const rand = PointDistribution.random(bbox, 100);
+// ---- Helper Function ----
+function inBounds(t, x, low, high) {
+    const msg = `Value ${x} should be in bouds between ${low} and ${high}`;
+    t.ok(x >= low && x <= high, msg);
+}
+
+test("Random points are in bounds", function(t) {
+    const bbox = new Rectangle(Vector.zero(), 10, 10);
+    const randPoints = PointDistribution.random(bbox, 10);
+
+    for (let point of randPoints) {
+        inBounds(t, point.x, bbox.x, bbox.x + bbox.width);
+        inBounds(t, point.y, bbox.y, bbox.y + bbox.height);
+    }
+
+    t.end();
+});
