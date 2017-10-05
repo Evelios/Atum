297bdd2641ec72808bedee059de77b6d2630ca63
Thomas Waters <twaters117@gmail.com>
2017-10-05 15:28:06 -0400

    PointDistribution Fix & Documentation

--- src/utilities/PointDistribution.js
+++ src/utilities/PointDistribution.js
@@ -11,24 +11,23 @@
 "use strict";
 
 import Vector from "../geometry/Vector";
+import Rectangle from "../geometry/Rectangle";
 import Rand from "./Rand";
-
-// Depends on the Rectangle Class & the Vector CLass
 
 /**
  * Creates a random distribution of points in a particular bounding box
- * with a particular average distance of between points.
+ * with a particular average distance between points.
  * 
  * @export
  * @param {Rectangle} bbox The bounding box to create the points in
- * @param {Number} d Average distance between points
- * 
+ * @param {number} d Average distance between points
  * @returns {Vector[]} The list of randomly distributed points
+ * @memberof PointDistribution
  */
 export function random(bbox, d) {
     let points = [];
 
-    const nPoints = bbox.area / d;
+    const nPoints = bbox.area / (d * d);
     for (let i = 0; i < nPoints; i++) {
         points.push(Rand.vector(bbox));
     }
@@ -36,49 +35,140 @@ export function random(bbox, d) {
     return points;
 }
 
+/**
+ * Creates a blue noise distribution of points in a particular bounding box
+ * with a particular average distance between points. This is done by
+ * creating a grid system and picking a random point in each grid. This has
+ * the effect of creating a less random distribution of points. The second
+ * parameter m determins the spacing between points in the grid. This ensures
+ * that no two points are in the same grid.
+ * 
+ * @summary Create a grid based random blue noise point distribution.
+ * 
+ * @export
+ * @param {Rectangle} bbox The bounding box to create the points in
+ * @param {number} d Average distance between points
+ * @param {number} m Maximum distance away from the center of the grid that a
+ *  point can be placed. This acts to increase the padding between points. 
+ *  This makes the noise less random. This number must be smaller than d.
+ * @returns {Vector[]} The list of randomly distributed points
+ * @memberof PointDistribution
+ */
 export function blueNoise(bbox, d) {
     throw "Error: Not Implemented";
 }
 
+/**
+ * Creates a poisson or blue noise distribution of points in a particular
+ * bounding box with a particular average distance between points. This is
+ * done by using poisson disk sampling which tries to create points so that the
+ * distance between neighbors is as close to a fixed number (the distance d)
+ * as possible.
+ * 
+ * @summary Create a blue noise distribution of points using poisson disk
+ *  sampling.
+ * 
+ * @export
+ * @param {Rectangle} bbox The bounding box to create the points in
+ * @param {number} d Average distance between points
+ * @returns {Vector[]} The list of randomly distributed points
+ * 
+ * @memberof PointDistribution
+ */
 export function poisson(bbox, d) {
     throw "Error: Not Implemented";
 }
 
+/**
+ * Creates a blue noise distribution of points in a particular bounding box
+ * with a particular average distance between points. This is done by using
+ * recursive wang tiles to create this distribution of points.
+ * 
+ * @summary Not Implemented Yet
+ * 
+ * @export
+ * @param {Rectangle} bbox The bounding box to create the points in
+ * @param {number} d Average distance between points
+ * @returns {Vector[]} The list of randomly distributed points
+ * @memberof PointDistribution
+ */
 export function recursiveWang(bbox, d) {
     throw "Error: Not Implemented";
 }
 
+/**
+ * Creates a square grid like distribution of points in a particular bounding
+ * box with a particular distance between points.
+ * 
+ * @export
+ * @param {Rectangle} bbox The bounding box to create the points in
+ * @param {number} d Average distance between points
+ * @returns {Vector[]} The list of randomly distributed points
+ * @memberof PointDistribution
+ */
 export function square(bbox, d) {
-    const dx = dy = d / 2;
+    const dx = d / 2;
+    const dy = dx;
     let points = [];
 
-    for (let y = 0; y < bbox.height / d; y++) {
-        for (let x = 0; x < bbox.width / d; x++) {
-            points.push(new Vector(dx + x * d, dy + y * d));
+    for (let y = 0; y < bbox.height; y += d) {
+        for (let x = 0; x < bbox.width; x += d) {
+            points.push(new Vector(dx + x, dy + y));
         }
     }
 
     return points;
 }
 
-export function hexagons(bbox, d, flatTop = true, w, h) {
-    // Temporary, Need to allow for the change of height and width
-    w,
-    h = d;
+/**
+ * Creates a uniform hexagonal distribution of points in a particular bounding
+ * box with a particular distance between points. The hexagons can also be
+ * specified to have a particular width or height as well as creating hexagons
+ * that have "pointy" tops or "flat" tops. By default it makes flat tops.
+ * 
+ * @export
+ * @param {Rectangle} bbox The bounding box to create the points in
+ * @param {number} d Average distance between points
+ * @param {boolean} [flatTop=true] Create hecagons with flat tops by default.
+ *  Otherwise go with the pointy top hexagons.
+ * @param {number} w The width of the hexagon tiles
+ * @param {number} h The height of the hexagon tiles
+ * @returns {Vector[]} The list of randomly distributed points
+ * @memberof PointDistribution
+ */
+export function hexagon(bbox, d, flatTop = true, w, h) {
+    // Need to allow for the change of height and width
+    // Running into "Uncaught Voronoi.closeCells() > this makes no sense!"
 
-    const dx = dy = d / 2;
+    const dx = d / 2;
+    const dy = dx;
     let points = [];
-
-    for (let y = 0; y < bbox.height / d; y++) {
-        for (let x = 0; x < bbox.width / d; x++) {
-            points.push(new Vector((x % 1) * dx + x * d,
-                (y % 1) * dy + y * d));
+    const altitude = Math.sqrt(3) / 2 * d;
+    var N = Math.sqrt(bbox.area / (d * d));
+    for (let y = 0; y < N; y++) {
+        for (let x = 0; x < N; x++) {
+            points.push(new Vector((0.5 + x) / N * bbox.width,
+                (0.25 + 0.5 * x % 2 + y) / N * bbox.height));
+            // points.push(new Vector((y % 2) * dx + x * d + dx, y * d + dy)); // Pointy Top
+            // points.push(new Vector(x * d, (x % 2) * dx + y * d)); // Flat Top
         }
     }
 
     return points;
 }
 
+/**
+ * Creates a circular distribution of points in a particular bounding box
+ * with a particular average distance between points.
+ * 
+ * @summary Not Implemented Yet
+ * 
+ * @export
+ * @param {Rectangle} bbox The bounding box to create the points in
+ * @param {number} d Average distance between points
+ * @returns {Vector[]} The list of randomly distributed points
+ * @memberof PointDistribution
+ */
 export function circular(bbox, d) {
     throw "Error: Not Implemented";
 }
